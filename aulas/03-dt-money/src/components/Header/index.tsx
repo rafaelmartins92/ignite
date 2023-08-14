2e144a9712ac6680242bc19583740af214ee90ea
rafaelmartins92 <rafael.martins92@outlook.com>
2023-08-13 22:08:46 -0300

    chore: create an accessible modal

--- aulas/03-dt-money/src/components/Header/index.tsx
+++ aulas/03-dt-money/src/components/Header/index.tsx
@@ -1,12 +1,27 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import { HeaderContainer, HeaderContent, NewTransactionButton } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+
 export function Header() {
   return (
     <HeaderContainer>
       <HeaderContent>
         <img src={logoImg} />
-        <NewTransactionButton>New transaction</NewTransactionButton>
+
+        <Dialog.Root>
+          <Dialog.Trigger asChild>
+            <NewTransactionButton>New transaction</NewTransactionButton>
+          </Dialog.Trigger>
+          <Dialog.Portal>
+            <Dialog.Overlay />
+            <Dialog.Content>
+              <Dialog.Title>New transaction</Dialog.Title>
+              <Dialog.Close />
+            </Dialog.Content>
+          </Dialog.Portal>
+        </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
   );
