eb3875acd7fe04da64e9efcb01f32a77398e6857
rafaelmartins92 <rafael.martins92@outlook.com>
2023-08-28 22:02:39 -0300

    chore: add next image

--- aulas/04-ignite-shop/src/pages/_app.tsx
+++ aulas/04-ignite-shop/src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import { AppProps } from 'next/app';
-import { globalStyles } from '../styles/global';
+import Image from 'next/image';
 
-import logoImg from '../assets/logo.svg'
+import logoImg from '../assets/logo.svg';
+
+import { globalStyles } from '../styles/global';
 import { Container, Header } from '../styles/pages/app';
 
 globalStyles();
@@ -10,7 +12,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={logoImg.src} alt="" />
+        <Image src={logoImg.src} width={52} height={52} alt="" />
       </Header>
       <Component {...pageProps} />
     </Container>
