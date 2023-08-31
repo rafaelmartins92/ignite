a929b74843442edb55ce826f092e1ea092804305
rafaelmartins92 <rafael.martins92@outlook.com>
2023-08-30 22:20:43 -0300

    chore: use ssg

--- aulas/04-ignite-shop/src/pages/index.tsx
+++ aulas/04-ignite-shop/src/pages/index.tsx
@@ -1,16 +1,13 @@
+import { GetStaticProps } from 'next';
 import Image from 'next/image';
+import Stripe from 'stripe';
 
 import { useKeenSlider } from 'keen-slider/react';
 import 'keen-slider/keen-slider.min.css';
 
 import { stripe } from '../lib/stripe';
-import camiseta1 from '../assets/camisetas/1.png';
-import camiseta2 from '../assets/camisetas/2.png';
-import camiseta3 from '../assets/camisetas/3.png';
 
 import { HomeContainer, Product } from '../styles/pages/home';
-import { GetServerSideProps } from 'next';
-import Stripe from 'stripe';
 
 interface HomeProps {
   products: {
@@ -46,7 +43,7 @@ export default function Home(props: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ['data.default_price'],
   });
@@ -66,5 +63,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 2, // 2 hours
   };
 };
