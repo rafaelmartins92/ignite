5ce81cc8a999867b160fe6457097ffbc29d2c551
rafaelmartins92 <rafael.martins92@outlook.com>
2023-08-31 22:12:51 -0300

    chore: ssg with dynamic paths

--- aulas/04-ignite-shop/src/pages/product/[id].tsx
+++ aulas/04-ignite-shop/src/pages/product/[id].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from 'next';
+import { GetStaticPaths, GetStaticProps } from 'next';
 import Image from 'next/image';
 import Stripe from 'stripe';
 
@@ -38,6 +38,17 @@ export default function Product({ product }: ProductProps) {
     </ProductContainer>
   );
 }
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [
+      {
+        params: { id: 'prod_OXiak8VOgYAdY4' },
+      },
+    ],
+    fallback: false,
+  };
+};
 
 export const getStaticProps: GetStaticProps<any, { id: string }> = async ({
   params,
