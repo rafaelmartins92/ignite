d1653bfcbe5edf43b48cb548bd97349f94eafaed
rafaelmartins92 <rafael.martins92@outlook.com>
2023-09-03 22:26:50 -0300

    chore: create success page structure

--- aulas/04-ignite-shop/src/pages/success.tsx
+++ aulas/04-ignite-shop/src/pages/success.tsx
@@ -1,7 +1,19 @@
+import Link from 'next/link';
+import { ImageContainer, SuccessContainer } from '../styles/pages/success';
+
 export default function Success() {
   return (
-    <h1>
-      Success
-    </h1>
-  )
+    <SuccessContainer>
+      <h1>Purchase made!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Rafael</strong>, sua{' '}
+        <strong>Camise Beyond the Limts</strong> já está a caminha da sua casa.
+      </p>
+
+      <Link href={'/'}>Back to catalog</Link>
+    </SuccessContainer>
+  );
 }
