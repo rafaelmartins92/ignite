b09c88afc4b32cbf03bfd3640a595ab41f9c564f
rafaelmartins92 <rafael.martins92@outlook.com>
2023-09-04 07:13:24 -0300

    chore: add favicon

--- aulas/04-ignite-shop/src/pages/_app.tsx
+++ aulas/04-ignite-shop/src/pages/_app.tsx
@@ -5,16 +5,28 @@ import logoImg from '../assets/logo.svg';
 
 import { globalStyles } from '../styles/global';
 import { Container, Header } from '../styles/pages/app';
+import Head from 'next/head';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Container>
-      <Header>
-        <Image src={logoImg.src} width={52} height={52} alt="" />
-      </Header>
-      <Component {...pageProps} />
-    </Container>
+    <>
+      <Head>
+        <link
+          rel="icon"
+          href="/favicon.svg"
+          type="image/svg"
+          sizes="32x32"
+        />
+      </Head>
+
+      <Container>
+        <Header>
+          <Image src={logoImg.src} width={52} height={52} alt="" />
+        </Header>
+        <Component {...pageProps} />
+      </Container>
+    </>
   );
 }
