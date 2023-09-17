01912f5753a47e08befed5f23b22cc0fa53f10e0
rafaelmartins92 <rafael.martins92@outlook.com>
2023-09-17 19:33:37 -0300

    chore: create story button

--- aulas/05-design-system/packages/react/src/index.tsx
+++ aulas/05-design-system/packages/react/src/index.tsx
@@ -1,12 +1,31 @@
+import { ComponentProps } from 'react'
+
 import { styled } from './styles'
 
-const Button = styled('button', {
+export const Button = styled('button', {
   fontFamily: '$default',
   backgroundColor: '$primary500',
-  borderRadius: '$md',
-  padding: '$4',
+  borderRadius: '$sm',
+  border: 0,
+  fontWeight: 'bold',
+  color: '$white',
+
+  variants: {
+    size: {
+      small: {
+        fontSize: 14,
+        padding: '$2 $4',
+      },
+      big: {
+        fontSize: 16,
+        padding: '$3 $6',
+      },
+    },
+  },
+
+  defaultVariants: {
+    size: 'small',
+  },
 })
 
-export function App() {
-  return <Button>App</Button>
-}
+export type ButtonProps = ComponentProps<typeof Button>
