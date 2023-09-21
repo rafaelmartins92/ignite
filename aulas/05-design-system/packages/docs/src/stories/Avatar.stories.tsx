e2448219248522c103d778aca81cd3fe9ffebd87
rafaelmartins92 <rafael.martins92@outlook.com>
2023-09-20 22:08:36 -0300

    chore: create component avatar

---
+++ aulas/05-design-system/packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,19 @@
+import type { StoryObj, Meta } from '@storybook/react'
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/rafaelmartins92.png',
+    alt: 'Rafael Martins',
+  },
+} as Meta
+
+export const Primary: StoryObj<AvatarProps> = {}
+
+export const WithFallback: StoryObj<AvatarProps> = {
+  args: {
+    src: undefined,
+  },
+}
