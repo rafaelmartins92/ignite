4ed2cccbcf2fa2fbee372eb821c13c0b8f297678
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-04 20:53:28 -0300

    chore: add home initial structure

---
+++ aulas/06-ignite-call/src/pages/home/index.tsx
@@ -0,0 +1,32 @@
+import { Heading, Text } from '@orafadev-ignite-ui/react'
+import Image from 'next/image'
+
+import previewImage from '../../assets/app-preview.png'
+
+import { Container, Hero, Preview } from './styles'
+
+export default function Home() {
+  return (
+    <Container>
+      <Hero>
+        <Heading as="h1" size="4xl">
+          Simplified scheduling
+        </Heading>
+        <Text size="xl">
+          Connect your calendar and allow people to schedule appointments during
+          your free time.
+        </Text>
+      </Hero>
+
+      <Preview>
+        <Image
+          src={previewImage}
+          alt="Calendar App preview"
+          height={400}
+          quality={100}
+          priority
+        />
+      </Preview>
+    </Container>
+  )
+}
--- aulas/06-ignite-call/src/pages/index.page.tsx
+++ aulas/06-ignite-call/src/pages/index.page.tsx
@@ -1,10 +1,1 @@
-import { Heading, Text } from '@orafadev-ignite-ui/react'
-
-export default function Home() {
-  return (
-    <div>
-      <Heading as="h1">Hello World!</Heading>
-      <Text>Hello World!</Text>
-    </div>
-  )
-}
+export { default } from './home'
