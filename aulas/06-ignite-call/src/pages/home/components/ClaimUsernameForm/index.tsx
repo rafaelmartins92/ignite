b8ba269fba0cdcaf4e1f95cf78026d1e88eef98d
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-06 17:55:41 -0300

    chore: add react hook form

--- aulas/06-ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
+++ aulas/06-ignite-call/src/pages/home/components/ClaimUsernameForm/index.tsx
@@ -1,13 +1,33 @@
 import { Button, TextInput } from '@orafadev-ignite-ui/react'
-import { Form } from './styles'
 import { ArrowRight } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+
+import { Form } from './styles'
+import { z } from 'zod'
+
+const claimUsernameFormSchema = z.object({
+  username: z.string(),
+})
+
+type ClaimUsernameFormData = z.infer<typeof claimUsernameFormSchema>
 
 export function ClaimUsernameForm() {
+  const { register, handleSubmit } = useForm<ClaimUsernameFormData>()
+
+  async function handlePreRegister(data: ClaimUsernameFormData) {
+    console.log(data)
+  }
+
   return (
-    <Form as="form">
-      <TextInput size="sm" prefix="orafadev.com/" placeholder="your-user" />
+    <Form as="form" onSubmit={handleSubmit(handlePreRegister)}>
+      <TextInput
+        size="sm"
+        prefix="orafadev.com/"
+        placeholder="your-user"
+        {...register('username')}
+      />
       <Button size="sm" type="submit">
-        Reserve
+        Schedule
         <ArrowRight />
       </Button>
     </Form>
