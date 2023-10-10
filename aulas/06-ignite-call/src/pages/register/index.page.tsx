97594cef417505f15ce84c76e79cfccd471c1852
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-09 21:04:46 -0300

    chore: persist user on prisma

--- aulas/06-ignite-call/src/pages/register/index.page.tsx
+++ aulas/06-ignite-call/src/pages/register/index.page.tsx
@@ -12,6 +12,8 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
+import { api } from '../lib/axios'
+
 import { Container, Form, FormError, Header } from './styles'
 
 const registerFormSchema = z.object({
@@ -46,7 +48,14 @@ export default function Register() {
   }, [router.query?.username, setValue])
 
   async function handleRegister(data: RegisterFormData) {
-    console.log(data)
+    try {
+      await api.post('/users', {
+        name: data.name,
+        username: data.username,
+      })
+    } catch (err) {
+      console.log(err)
+    }
   }
 
   return (
