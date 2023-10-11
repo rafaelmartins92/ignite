6e93cb0ae85a03b5da3c3ff495ae68bced39fc6b
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-10 21:38:53 -0300

    chore: create connect calendar page

---
+++ aulas/06-ignite-call/src/pages/register/connect-calendar/index.page.tsx
@@ -0,0 +1,36 @@
+import { Button, Heading, MultiStep, Text } from '@orafadev-ignite-ui/react'
+import { ArrowRight } from 'phosphor-react'
+
+import { ConnectBox, ConnectItem } from './styles'
+
+import { Container, Header } from '../styles'
+
+export default function Register() {
+  // async function handleRegister(data: RegisterFormData) {}
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Connect your calendar!</Heading>
+        <Text>
+          Connect your calendar to automatically check busy hours and new events
+          as they are scheduled.
+        </Text>
+
+        <MultiStep size={4} currentStep={2} />
+      </Header>
+
+      <ConnectBox>
+        <ConnectItem>
+          <Text>Google Calendar</Text>
+          <Button variant="secondary" size="sm">
+            Connect <ArrowRight />
+          </Button>
+        </ConnectItem>
+        <Button type="submit">
+          Next step <ArrowRight />
+        </Button>
+      </ConnectBox>
+    </Container>
+  )
+}
