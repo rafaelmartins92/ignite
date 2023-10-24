78db0d5f61fe3f08f32a905f7246c3925e3b9427
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-24 08:11:07 -0300

    style: add calendar styles

--- aulas/06-ignite-call/src/components/Calendar/index.tsx
+++ aulas/06-ignite-call/src/components/Calendar/index.tsx
@@ -35,24 +35,47 @@ export function Calendar() {
               <th key={weekDay}>{weekDay}.</th>
             ))}
           </tr>
-          <tbody>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td>
-                <CalendarDay>1</CalendarDay>
-              </td>
-              <td>
-                <CalendarDay>2</CalendarDay>
-              </td>
-              <td>
-                <CalendarDay>3</CalendarDay>
-              </td>
-            </tr>
-          </tbody>
         </thead>
+        <tbody>
+          <tr>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay disabled>2</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>3</CalendarDay>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+            <td>
+              <CalendarDay>1</CalendarDay>
+            </td>
+          </tr>
+        </tbody>
       </CalendarBody>
     </CalendarContainer>
   )
