9fcf091c4dcdfe9e51e792239d0056c30ee392ae
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-24 21:07:03 -0300

    style: add hour list visual structure

--- aulas/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ aulas/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,11 +1,43 @@
 import { Calendar } from '@/src/components/Calendar'
 
-import { Container } from './styles'
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from './styles'
 
 export function CalendarStep() {
+  const isDateSelected = true
+
   return (
-    <Container>
+    <Container isTimePickerOpen={isDateSelected}>
       <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            monday <span>September 20th</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>00:00h</TimePickerItem>
+            <TimePickerItem>01:00h</TimePickerItem>
+            <TimePickerItem>02:00h</TimePickerItem>
+            <TimePickerItem>03:00h</TimePickerItem>
+            <TimePickerItem>04:00h</TimePickerItem>
+            <TimePickerItem>05:00h</TimePickerItem>
+            <TimePickerItem>06:00h</TimePickerItem>
+            <TimePickerItem>07:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>09:00h</TimePickerItem>
+            <TimePickerItem>10:00h</TimePickerItem>
+            <TimePickerItem>11:00h</TimePickerItem>
+            <TimePickerItem>12:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
     </Container>
   )
 }
