b2da6e8700034e34aec4b9ad5f290dbcbe78a1cd
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-26 19:41:04 -0300

    chore: calculate month days

--- aulas/06-ignite-call/src/components/Calendar/index.tsx
+++ aulas/06-ignite-call/src/components/Calendar/index.tsx
@@ -1,6 +1,6 @@
 import { CaretLeft, CaretRight } from 'phosphor-react'
 import { getWeekDays } from '@/src/utils/get-week-days'
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import dayjs from 'dayjs'
 
 import {
@@ -33,6 +33,28 @@ export function Calendar() {
 
   const currentMonth = currentDate.format('MMMM')
   const currentYear = currentDate.format('YYYY')
+
+  const calendarWeeks = useMemo(() => {
+    const daysInMonthArray = Array.from({
+      length: currentDate.daysInMonth(),
+    }).map((_, i) => {
+      return currentDate.set('date', i + 1)
+    })
+
+    const firstWeekDay = currentDate.get('day')
+
+    const previousMonthFillArray = Array.from({
+      length: firstWeekDay,
+    })
+      .map((_, i) => {
+        return currentDate.subtract(i + 1, 'day')
+      })
+      .reverse()
+
+    return [...previousMonthFillArray, ...daysInMonthArray]
+  }, [currentDate])
+
+  console.log(calendarWeeks)
 
   return (
     <CalendarContainer>
