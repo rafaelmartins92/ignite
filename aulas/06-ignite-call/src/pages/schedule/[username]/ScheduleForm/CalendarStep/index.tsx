ee7fb38ade681d1456a52eeeeb22df087aabcaa6
rafaelmartins92 <rafael.martins92@outlook.com>
2023-10-29 18:28:00 -0300

    chore: load available times

--- aulas/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
+++ aulas/06-ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 import { Calendar } from '@/src/components/Calendar'
+import { api } from '@/src/lib/axios'
 import dayjs from 'dayjs'
 
 import {
@@ -12,13 +14,33 @@ import {
 
 export function CalendarStep() {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
+  const [availability, setAvailability] = useState(null)
+
+  const router = useRouter()
 
   const isDateSelected = !!selectedDate
+  const username = String(router.query.username)
 
   const weekDay = selectedDate ? dayjs(selectedDate).format('dddd') : null
   const describedDate = selectedDate
     ? dayjs(selectedDate).format('MMMM D')
     : null
+
+  useEffect(() => {
+    if (!selectedDate) {
+      return
+    }
+
+    api
+      .get(`/users/${username}/availability`, {
+        params: {
+          date: dayjs(selectedDate).format('YYYY-MM-DD'),
+        },
+      })
+      .then((response) => {
+        console.log(response.data)
+      })
+  }, [selectedDate, username])
 
   return (
     <Container isTimePickerOpen={isDateSelected}>
