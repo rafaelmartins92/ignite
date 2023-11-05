3acc44554f8ac14bd712e2160d70fd167d88119a
rafaelmartins92 <rafael.martins92@outlook.com>
2023-11-05 19:36:22 -0300

    chore: create google event

--- aulas/06-ignite-call/src/pages/api/users/[username]/schedule.api.ts
+++ aulas/06-ignite-call/src/pages/api/users/[username]/schedule.api.ts
@@ -2,8 +2,10 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import { z } from 'zod'
 import dayjs from 'dayjs'
+import { google } from 'googleapis'
 
-import { prisma } from '../../../../lib/prisma'
+import { prisma } from '@/src/lib/prisma'
+import { getGoogleOAuthToken } from '@/src/lib/google'
 
 export default async function handler(
   req: NextApiRequest,
@@ -56,13 +58,42 @@ export default async function handler(
     })
   }
 
-  await prisma.scheduling.create({
+  const scheduling = await prisma.scheduling.create({
     data: {
       name,
       email,
       observations,
       date: schedulingDate.toDate(),
       user_id: user.id,
+    },
+  })
+
+  const calendar = google.calendar({
+    version: 'v3',
+    auth: await getGoogleOAuthToken(user.id),
+  })
+
+  await calendar.events.insert({
+    calendarId: 'primary',
+    conferenceDataVersion: 1,
+    requestBody: {
+      summary: `Ignite Call: ${name}`,
+      description: observations,
+      start: {
+        dateTime: schedulingDate.format(),
+      },
+      end: {
+        dateTime: schedulingDate.add(1, 'hour').format(),
+      },
+      attendees: [{ email, displayName: name }],
+      conferenceData: {
+        createRequest: {
+          requestId: scheduling.id,
+          conferenceSolutionKey: {
+            type: 'hangoutsMeet',
+          },
+        },
+      },
     },
   })
 
