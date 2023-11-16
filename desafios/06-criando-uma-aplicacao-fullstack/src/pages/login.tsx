bbb3c33a679e1155ef5ff4e9c5a948d0324d95ea
rafaelmartins92 <rafael.martins92@outlook.com>
2023-11-16 20:50:52 -0300

    feat: add next auth google

--- desafios/06-criando-uma-aplicacao-fullstack/src/pages/login.tsx
+++ desafios/06-criando-uma-aplicacao-fullstack/src/pages/login.tsx
@@ -1,20 +1,27 @@
-import { AuthButtons } from "@/components/AuthButtons";
-import { Heading, Text } from "@/components/Typography";
-import { LoginContainer, LoginSection, WelcomeSection } from "@/styles/pages/login";
-import Image from "next/image";
+import { AuthButtons } from '@/components/AuthButtons';
+import { Heading, Text } from '@/components/Typography';
+import { LoginContainer, LoginSection, WelcomeSection } from '@/styles/pages/login';
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Login() {
   return (
     <LoginContainer>
+      <Head>
+        <title>Login | BookWise</title>
+        <link rel="shortcut icon" href="/favicon.svg" type="image/svg" />
+      </Head>
       <LoginSection>
         <Image src="/images/logo.svg" alt="BookWise Logo" width={232} height={58} />
       </LoginSection>
 
       <WelcomeSection>
-        <Heading size="lg" color="gray-100">Welcome!</Heading>
+        <Heading size="lg" color="gray-100">
+          Welcome!
+        </Heading>
         <Text color="gray-200">Log in or access as a guest.</Text>
         <AuthButtons />
       </WelcomeSection>
     </LoginContainer>
-  )
+  );
 }
