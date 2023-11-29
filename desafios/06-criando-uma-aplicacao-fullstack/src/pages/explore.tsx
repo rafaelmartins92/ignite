85e562d7cea0364ab0bb7fad740da75102829090
rafaelmartins92 <rafael.martins92@outlook.com>
2023-11-28 22:32:01 -0300

    chore: add categories tags

--- desafios/06-criando-uma-aplicacao-fullstack/src/pages/explore.tsx
+++ desafios/06-criando-uma-aplicacao-fullstack/src/pages/explore.tsx
@@ -1,15 +1,39 @@
-import { useState } from 'react';
+import { ReactElement, useState } from 'react';
 import { Binoculars, MagnifyingGlass } from '@phosphor-icons/react';
 import { BooksGrid, ExploreContainer, TagsContainer } from '@/styles/pages/explore';
 import { DefaultLayout } from '@/layouts/DefaultLayout';
 import { PageTitle } from '@/components/ui/PageTitle';
 import { Input } from '@/components/ui/Form/Input';
 import { Tag } from '@/components/ui/Tag';
-import { BookCard } from '@/components/BookCard';
+import { BookCard, BookWithAvgRating } from '@/components/BookCard';
+import { useQuery } from '@tanstack/react-query';
+import { api } from '@/lib/axios';
+import { Category } from '@prisma/client';
 import { NextPageWithLayout } from './_app';
 
 const ExplorePage: NextPageWithLayout = () => {
   const [search, setSearch] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
+  const { data: categories } = useQuery<Category[]>(['categories'], async () => {
+    const { data } = await api.get('/books/categories');
+    return data?.categories ?? [];
+  });
+
+  const { data: books } = useQuery<BookWithAvgRating[]>(['books', selectedCategory], async () => {
+    const { data } = await api.get('/books', {
+      params: {
+        category: selectedCategory,
+      },
+    });
+    return data?.books ?? [];
+  });
+
+  const filteredBooks = books?.filter((book) => {
+    return (
+      book.name.toLowerCase().includes(search.toLowerCase()) || book.author.toLowerCase().includes(search.toLowerCase())
+    );
+  });
 
   return (
     <ExploreContainer>
@@ -19,29 +43,40 @@ const ExplorePage: NextPageWithLayout = () => {
         <Input
           placeholder="Search by book or author"
           icon={<MagnifyingGlass size={20} />}
-          css={{ maxWidth: 433 }}
+          css={{
+            maxWidth: '433px',
+          }}
           value={search}
           onChange={({ target }) => setSearch(target.value)}
         />
       </header>
 
       <TagsContainer>
-        <Tag>Education</Tag>
+        <Tag active={selectedCategory === null} onClick={() => setSelectedCategory(null)}>
+          Tudo
+        </Tag>
+        {categories?.map((category, i) => (
+          <Tag
+            key={category?.id}
+            active={selectedCategory === category.id}
+            onClick={() => setSelectedCategory(category.id)}
+          >
+            {category?.name}
+          </Tag>
+        ))}
       </TagsContainer>
 
       <BooksGrid>
-        {/* <BookCard />`` */}
+        {filteredBooks?.map((book) => (
+          <BookCard key={book.id} size="lg" book={book} />
+        ))}
       </BooksGrid>
     </ExploreContainer>
   );
 };
 
-ExplorePage.getLayout = (page) => {
-  return (
-    <div>
-      <DefaultLayout title="Explore">{page}</DefaultLayout>
-    </div>
-  );
+ExplorePage.getLayout = (page: ReactElement) => {
+  return <DefaultLayout title="Explore">{page}</DefaultLayout>;
 };
 
 export default ExplorePage;
