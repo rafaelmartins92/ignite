7f2736a0f36ef75dababac5029e034491fa3fc9a
rafaelmartins92 <rafael.martins92@outlook.com>
2023-12-11 08:04:07 -0300

    chore: add profile api routes

--- desafios/06-criando-uma-aplicacao-fullstack/src/pages/profile/[id].tsx
+++ desafios/06-criando-uma-aplicacao-fullstack/src/pages/profile/[id].tsx
@@ -6,14 +6,28 @@ import { HomeContainer } from '@/styles/pages/home';
 import { ProfileRatings } from '@/components/ProfileRatings';
 import { useQuery } from '@tanstack/react-query';
 import { api } from '@/lib/axios';
+import { Rating } from '@prisma/client';
 
 import { NextPageWithLayout } from '../_app';
+
+export type ProfileData = {
+  user: {
+    avatar_url: string;
+    name: string;
+    member_since: string;
+  };
+  rating: Rating[];
+  readPages: number;
+  ratedBooks: number;
+  readAuthors: number;
+  mostReadCategory?: string;
+};
 
 const ProfilePage: NextPageWithLayout = () => {
   const router = useRouter();
   const userId = router.query.id as string;
 
-  const { data: profile } = useQuery(
+  const { data: profile } = useQuery<ProfileData>(
     ['profile', userId],
     async () => {
       const { data } = await api.get(`/profile/${userId}`);
@@ -24,9 +38,17 @@ const ProfilePage: NextPageWithLayout = () => {
     }
   );
 
+  console.log(profile);
+
   return (
     <HomeContainer>
-      <ProfileRatings />
+      {!!profile ? (
+        <>
+          <ProfileRatings />
+        </>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </HomeContainer>
   );
 };
